799c2842d23e600a91d125e93058834e47d1a539
Kurze <Marcelo_andre2003@ghotmail.com>
2024-12-04 18:44:26 -0600

    Did a lot of unnecesary stuff just to change the name from "Awakened Kurze" to "Awakened Kurzes" lol

---
+++ src/main/java/org/kurze/awakened_kurzes/Awakened_Kurzes.java
@@ -0,0 +1,81 @@
+package org.kurze.awakened_kurzes;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.kurze.awakened_kurzes.item.ModItems;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(org.kurze.awakened_kurzes.Awakened_Kurzes.MODID)
+public class Awakened_Kurzes {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "awakened_kurzes";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "awakened_kurzes" namespace
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    
+    //-----------------------LISTA DE REGISTROS---------------------------------------------------------------------
+    public Awakened_Kurzes() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup); // TELLS FORGE TO REGISTER THE MOD
+
+        BLOCKS.register(modEventBus); // REGISTER MOD BLOCKS
+        ModItems.register(modEventBus); // REGISTER MOD ITEMS
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    //-----------------------SEPA LA VERGA---------------------------------------------------------------------
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+    
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("THE KURZES ARE AWAKENING");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
---
+++ src/main/java/org/kurze/awakened_kurzes/item/ModItems.java
@@ -0,0 +1,22 @@
+package org.kurze.awakened_kurzes.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.kurze.awakened_kurzes.Awakened_Kurzes;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Awakened_Kurzes.MODID);
+    //-----------------------ITEMS---------------------------------------------------------------------
+
+    public static final RegistryObject<Item> KURZARITE = ITEMS.register("kurzarite",
+            ()->new Item(new Item.Properties()));
+
+    //-----------------------EVENT BUS---------------------------------------------------------------------
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
